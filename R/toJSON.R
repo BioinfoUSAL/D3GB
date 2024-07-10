a0f34a8adcd3a53165f02cc659c70fd71b51478d
BioinfoUSAL <bioinfo@usal.es>
2024-07-10 17:28:38 +0200

    2.1

--- R/toJSON.R
+++ R/toJSON.R
@@ -3,21 +3,20 @@ toJSON <- function(x){
   if(length(x)<=1){
     if(is.null(x)||identical(is.na(x),TRUE)){
       json <- "null"
-    }else{
-      if(is.data.frame(x)){
-	aux <- apply(x, 1, function(x)  paste0('[', toJSON(x), ']', collapse = ""))
-	aux <- paste0(aux , collapse = ",")
-	json <- paste0("[", aux, "]", collapse = "")
-      }else if(is.array(x)){
-	if(dim(x)[1]>0&&dim(x)[2]>0)
-	  aux <- toJSON(x[1,1])
-	else
-	  aux <- ""
-	json <- paste0("[",aux,"]", collapse = "")
-      }else if(is.factor(x)){
-	json <- paste0('"',x,'"')
-      }else if(is.vector(x)){
-	if(is.list(x)){
+    }else if(is.vector(x)){
+	if(is.numeric(x)){
+          if(length(x)==0)
+            json <- "[]"
+          else
+	    json <- x
+	}else if(is.logical(x)){
+	  if(identical(x,TRUE))
+	    json <- "true"
+	  else
+	    json <- "false"
+	}else if(is.character(x)){
+	  json <- paste0('"',x,'"')
+        }else if(is.list(x)){
 	  if(length(x)==0)
 	    json <- "{}"
 	  else{
@@ -28,27 +27,27 @@ toJSON <- function(x){
 	      json <- paste0("{", aux, "}", collapse = "")
 	    }
           }
-	}else if(is.character(x)){
-	  json <- paste0('"',x,'"')
-        }else if(is.logical(x)){
-	  if(identical(x,TRUE))
-	    json <- "true"
-	  else
-	    json <- "false"
-	}else if(is.numeric(x)){
-          if(length(x)==0)
-            json <- "[]"
-          else
-	    json <- x
 	}
-      }
+    }else if(is.factor(x)){
+	json <- paste0('"',x,'"')
+    }else if(is.array(x)){
+        aux <- "null"
+        if(length(dim)==1){
+          aux <- toJSON(x[1])
+        }else if(length(dim)==2 && dim(x)[1] > 0 && dim(x)[2] > 0){
+          aux <- toJSON(x[1,1])
+        }
+        json <- paste0("[",aux,"]", collapse = "")
+    }else if(is.data.frame(x)){
+	aux <- apply(x, 1, function(x)  paste0('[', toJSON(x), ']', collapse = ""))
+	aux <- paste0(aux , collapse = ",")
+	json <- paste0("[", aux, "]", collapse = "")
     }
-  } else {
-    if(is.data.frame(x)){      
+  }else if(is.data.frame(x)){
       aux <- lapply(seq_len(dim(x)[1]), function(x,z) paste0("[", paste0(lapply(seq_along(z[x,]), function(x,y) toJSON(y[[x]]), y=z[x,]), collapse = ","), "]", collapse = ""), z=x)
       aux <- paste0(aux , collapse = ",")
       json <- paste0("[", aux, "]", collapse = "")
-    }else if(is.list(x)){
+  }else if(is.list(x)){
       if(is.null(names(x))){
 	aux <- paste0(lapply(x, toJSON), collapse = ",")
 	json <- paste0("[", aux, "]", collapse = "")
@@ -57,14 +56,13 @@ toJSON <- function(x){
 	aux <- paste0(aux , collapse = ",")
 	json <- paste0("{", aux, "}", collapse = "")
       }      
-    }else if(is.array(x)){
+  }else if(is.array(x)){
       aux <- apply(x, 1, toJSON)
       aux <- paste0(aux, collapse = ",")
       json <- paste0("[", aux, "]", collapse = "")
-    }else if(is.vector(x)||is.factor(x)){
+  }else if(is.vector(x)||is.factor(x)){
       aux <- paste0(lapply(x, toJSON), collapse = ",")
       json <- paste0("[", aux, "]", collapse = "")
-    }
   }
   return(json)
 }
